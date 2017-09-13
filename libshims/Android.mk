fd4e46a791317c74b4abd210c2e3dec763293963
Ta Thanh Lam <tathanhlam66@gmail.com>
2017-09-13 10:09:25 +0700

    m7-common: Don't shim htc_log

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -28,16 +28,6 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    htc_log.c
-
-LOCAL_MODULE := liblog_shim
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
     wvm_shim.cpp
 
 LOCAL_SHARED_LIBRARIES := libstagefright_foundation
