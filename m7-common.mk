ffedcafa2be1e83ada1397ff1e4e4ab47da38a25
Ta Thanh Lam <tathanhlam66@gmail.com>
2017-09-16 03:38:48 +0700

    m7-common: hidl: Add Camera HAL interfaces package

--- m7-common.mk
+++ m7-common.mk
@@ -49,6 +49,11 @@ PRODUCT_PACKAGES += \
     libcamera_shim \
     Snap
 
+# Camera HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
+
 # Charger
 WITH_LINEAGE_CHARGER := false
 
