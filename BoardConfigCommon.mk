28b343de820bf41f4b61974061d31ccaa1939a22
Paul Keith <javelinanddart@gmail.com>
2018-02-15 23:33:28 +0100

    m7-common: Transition to TARGET_LD_SHIM_LIBS
     * Limits security exposure from shims
    
     * As a bonus, we no longer need noatsecure
       to make LD_SHIM_LIBS persist through services
    
    Change-Id: I877192422062f3e59c81a7130ad1a2b4be5d1647

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -147,6 +147,9 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 -include device/qcom/sepolicy/sepolicy.mk
 -include device/qcom/sepolicy/legacy-sepolicy.mk
 
+# Shims
+TARGET_LD_SHIM_LIBS := /system/bin/mpdecision|libshims_atomic.so:/system/lib/hw/camera.vendor.msm8960.so|libcamera_shim.so:/system/vendor/lib/libqc-opt.so|libqc-opt_shim.so:/system/lib/liblog.so|liblog_shim.so:/system/vendor/lib/libwvm.so|libshim_wvm.so:/system/lib/libril.so|libshim_ril.so:/system/vendor/lib/libril-qc-qmi-1.so|libshim_ril.so
+
 # USB
 TARGET_USES_LEGACY_ADB_INTERFACE := true
 
