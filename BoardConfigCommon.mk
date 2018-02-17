d27ef84645c30c0c1e3493202f772a92c877ba4d
Bruno Martins <bgcngm@gmail.com>
2018-02-17 18:25:31 +0000

    m7-common: Split shims list
    Change-Id: I242210227cca463a1d9c729339c4cf6b3c7c4a32

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,7 +150,13 @@ BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 -include device/qcom/sepolicy/legacy-sepolicy.mk
 
 # Shims
-TARGET_LD_SHIM_LIBS :=  /system/vendor/lib/hw/camera.vendor.msm8960.so|libcamera_shim.so:/system/vendor/lib/libqc-opt.so|libqc-opt_shim.so:/system/lib/liblog.so|liblog_shim.so:/system/vendor/lib/libwvm.so|libshim_wvm.so:/system/vendor/lib/libril.so|libshim_ril.so:/system/vendor/lib/libril-qc-qmi-1.so|libshim_ril.so
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/lib/hw/camera.vendor.msm8960.so|libcamera_shim.so \
+    /system/vendor/lib/libqc-opt.so|libqc-opt_shim.so \
+    /system/lib/liblog.so|liblog_shim.so \
+    /system/vendor/lib/libwvm.so|libshim_wvm.so \
+    /system/vendor/lib/libril.so|libshim_ril.so \
+    /system/vendor/lib/libril-qc-qmi-1.so|libshim_ril.so
 
 # USB
 TARGET_USES_LEGACY_ADB_INTERFACE := true
