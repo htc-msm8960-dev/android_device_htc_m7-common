d2f3c62e8cc0c4eb02f937e132f7ed33e8e32e93
gimme <gimmeitorilltell@gmail.com>
2018-04-09 00:21:21 -0400

    m7-common: Get the camera to find all libs

--- camera/Android.mk
+++ camera/Android.mk
@@ -21,33 +21,28 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES += \
-    $(LOCAL_PATH)/../include \
+LOCAL_C_INCLUDES := \
     framework/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
-LOCAL_STATIC_LIBRARIES := \
-    libarect \
-    libbase
-
 LOCAL_SHARED_LIBRARIES := \
     libhardware \
     liblog \
     libcamera_client \
+    libgui \
+    libhidltransport \
+    libsensor \
     libutils \
-    libcutils \
-    libdl \
-    android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
+    android.hidl.token@1.0-utils
 
-LOCAL_HEADER_LIBRARIES := libnativebase_headers
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
-
 include $(BUILD_SHARED_LIBRARY)
