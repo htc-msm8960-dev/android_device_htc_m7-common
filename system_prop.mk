69485282054653794c6bffb60056cd7db234e5f5
TARKZiM <tom8476oo@gmail.com>
2019-09-26 19:20:57 +0800

    Revert "[DNM] m7-common: Force enable ADB debugging"
    This reverts commit 5932933c5bad388ac1a173db20c4566544cffbad.

--- system_prop.mk
+++ system_prop.mk
@@ -17,9 +17,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2015-08-05
-
-# DEBUG
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.service.adb.enable=1  \
-	persist.service.debuggable=1  \
-	persist.sys.usb.config=mtp,adb
