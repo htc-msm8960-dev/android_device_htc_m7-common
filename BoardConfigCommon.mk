88c6ccb9e5560d3f87186ef72638e51a10922861
TARKZiM <tom8476oo@gmail.com>
2019-10-31 10:07:48 +0800

    m7-common: Include QCOM SEPolicy Legacy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,7 +82,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/m7-common/releasetools
 
 # SELinux
-#include device/qcom/sepolicy-legacy/sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 BOARD_SEPOLICY_DIRS += device/htc/m7-common/sepolicy-minimal
 
