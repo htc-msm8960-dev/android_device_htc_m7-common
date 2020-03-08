1af62c689af82d2e1a6dae227dbd42d331988b10
TARKZiM <tom8476oo@gmail.com>
2020-03-08 15:01:13 +0800

    [DNM] m7-common: Set SELinux to Permissive for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,7 @@ DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest.xml
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31
-# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := m7_defconfig
